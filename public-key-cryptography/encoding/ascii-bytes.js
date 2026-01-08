732e5f5fc24ec7ac5afd88e23c3c60c6633dc929
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-01-08 12:33:11 +0530

    ascii to bytes

---
+++ public-key-cryptography/encoding/ascii-bytes.js
@@ -0,0 +1,13 @@
+function asciiToBytes(asciiString){  
+    const byteArray = [];
+    for (let i = 0; i < asciiString.length; i++) {
+        byteArray.push(asciiString.charCodeAt(i));
+    }
+    return byteArray; 
+}
+
+// Example usage:
+const ascii = "Hello";
+const byteArray = asciiToBytes(ascii);
+console.log(byteArray)
+
