0dcf1e2475359b814efc3e0b76a452c865cd1fde
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-01-15 01:52:12 +0530

    added array to hex

--- public-key-cryptography/encoding/array-hex.js
+++ public-key-cryptography/encoding/array-hex.js
@@ -1,6 +1,12 @@
 function arrayToHex(byteArray){
     let hexString = '';
     for (let i = 0; i < byteArray.length; i++) {
-        
+        hexString += byteArray[i].toString(16).padStart(2, '0');
     }
+    return hexString;
 }
+
+//Example usage:
+const byteArray = new Uint8Array([72, 101, 108, 108, 111]);
+const hexString = arrayToHex(byteArray);
+console.log(hexString);
