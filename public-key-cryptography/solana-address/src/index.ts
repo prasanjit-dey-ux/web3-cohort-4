ab4219b4273d79bf855ce5c6bceb74f3f2c8adac
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-01-23 23:55:51 +0530

    updated solana adress generation

--- public-key-cryptography/solana-address/src/index.ts
+++ public-key-cryptography/solana-address/src/index.ts
@@ -2,3 +2,17 @@
 
 import * as ed from "@noble/ed25519";
 import bs58 from "bs58";
+
+// 1. Generate secret key (wallet identity)
+const secretKey = ed.utils.randomSecretKey();
+
+// 2. Derive public key (account identity)
+const publicKey =  await ed.getPublicKeyAsync(secretKey);
+
+// 3. Encode public key as Base58 (Solana address)
+const solanaAddress = bs58.encode(publicKey);
+
+// output
+console.log("Secret key (Bytes):", secretKey);
+console.log("Public key (bytes):", publicKey);
+console.log("Solana address:", solanaAddress);
